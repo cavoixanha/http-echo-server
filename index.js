f43ef5d65157feec1609dd9743c0016fe562501c
Nam Nguyen <kiemsi789@yahoo.com>
2019-08-13 10:56:32 +0700

    echo server
    Signed-off-by: Nam Nguyen <kiemsi789@yahoo.com>

--- index.js
+++ index.js
@@ -1,85 +1,139 @@
-#!/usr/bin/env node
-'use strict'
+// #!/usr/bin/env node
+// 'use strict'
 
-var getPort = require('get-port')
-var server = require('net').createServer()
+// var getPort = require('get-port')
+// var server = require('net').createServer()
 
-var cid = 0
+// var cid = 0
 
-module.exports = server // for testing
+// module.exports = server // for testing
 
-onEmit(server, { ignore: ['connection', 'listening', 'error'] }, function (eventName) {
-  console.log('[server] event:', eventName)
-})
+// onEmit(server, { ignore: ['connection', 'listening', 'error'] }, function (eventName) {
+//   console.log('[server] event:', eventName)
+// })
 
-server.on('connection', function (c) {
-  var gotData = false
-  var _cid = ++cid
+// server.on('connection', function (c) {
+//   var gotData = false
+//   var _cid = ++cid
 
-  console.log('[server] event: connection (socket#%d)', _cid)
+//   console.log('[server] event: connection (socket#%d)', _cid)
 
-  onEmit(c, { ignore: ['lookup', 'error'] }, function (eventName) {
-    console.log('[socket#%d] event:', _cid, eventName)
-  })
+//   onEmit(c, { ignore: ['lookup', 'error'] }, function (eventName) {
+//     console.log('[socket#%d] event:', _cid, eventName)
+//   })
 
-  c.on('lookup', function (err, address, family) {
-    if (err) {
-      console.log('[socket#%d] event: lookup (error: %s)', _cid, err.message)
-    } else {
-      console.log('[socket#%d] event: lookup (address: %s, family: %s)', _cid, address, family)
-    }
-  })
+//   c.on('lookup', function (err, address, family) {
+//     if (err) {
+//       console.log('[socket#%d] event: lookup (error: %s)', _cid, err.message)
+//     } else {
+//       console.log('[socket#%d] event: lookup (address: %s, family: %s)', _cid, address, family)
+//     }
+//   })
 
-  c.on('data', function (chunk) {
-    console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
-    if (!gotData) {
-      gotData = true
-      c.write('HTTP/1.1 200 OK\r\n')
-      c.write('Date: ' + (new Date()).toString() + '\r\n')
-      c.write('Connection: close\r\n')
-      c.write('Content-Type: text/plain\r\n')
-      c.write('Access-Control-Allow-Origin: *\r\n')
-      c.write('Access-Control-Request-Headers: *\r\n')
-      c.write('Access-Control-Allow-Methods: *\r\n')
-      c.write('Access-Control-Allow-Headers: *\r\n')
-      // c.write('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE\r\n')
-      // c.write('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, keyapi, content-type\r\n')
-      c.write('\r\n')
-      setTimeout(function () {
-        c.end()
-      }, 2000)
-    }
-    c.write(chunk.toString())
-  })
+//   c.on('data', function (chunk) {
+//     console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
+//     if (!gotData) {
+//       gotData = true
+//       // c.write('HTTP/1.1 200 OK\r\n')
+//       // c.write('Date: ' + (new Date()).toString() + '\r\n')
+//       // c.write('Connection: close\r\n')
+//       // c.write('Content-Type: text/plain\r\n')
+//       // c.write('Access-Control-Allow-Origin: *\r\n')
+//       // c.write('Access-Control-Request-Headers: *\r\n')
+//       // c.write('Access-Control-Allow-Methods: *\r\n')
+//       // c.write('Access-Control-Allow-Headers: *\r\n')
+//       // c.write('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE\r\n')
+//       // c.write('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, keyapi, content-type\r\n')
+//       // c.write('\r\n')
+//       c.writeHead(200, { 'Content-Type': 'application/json' });
+//       setTimeout(function () {
+//         c.end()
+//       }, 2000)
+//     }
+//     c.write(chunk.toString())
+//   })
 
-  c.on('error', function (err) {
-    console.log('[socket#%d] event: error (msg: %s)', _cid, err.message)
-  })
-})
+//   c.on('error', function (err) {
+//     console.log('[socket#%d] event: error (msg: %s)', _cid, err.message)
+//   })
+// })
 
-server.on('listening', function () {
-  var port = server.address().port
-  console.log('[server] event: listening (port: %d)', port)
-})
+// server.on('listening', function () {
+//   var port = server.address().port
+//   console.log('[server] event: listening (port: %d)', port)
+// })
 
-server.on('error', function (err) {
-  console.log('[server] event: error (msg: %s)', err.message)
-})
+// server.on('error', function (err) {
+//   console.log('[server] event: error (msg: %s)', err.message)
+// })
 
-var port = process.argv[2] || process.env.PORT
+// var port = process.argv[2] || process.env.PORT
 
-if (port) {
-  server.listen(port)
-} else {
-  getPort({ port: 3000 }).then(function (port) {
-    server.listen(port)
-  })
-}
+// if (port) {
+//   server.listen(port)
+// } else {
+//   getPort({ port: 3000 }).then(function (port) {
+//     server.listen(port)
+//   })
+// }
 
-function onEmit (emitter, opts, cb) {
-  var emitFn = emitter.emit
-  emitter.emit = function (eventName) {
-    if (opts.ignore.indexOf(eventName) === -1) cb.apply(null, arguments)
-    return emitFn.apply(emitter, arguments)
-  }
-}
+// function onEmit (emitter, opts, cb) {
+//   var emitFn = emitter.emit
+//   emitter.emit = function (eventName) {
+//     if (opts.ignore.indexOf(eventName) === -1) cb.apply(null, arguments)
+//     return emitFn.apply(emitter, arguments)
+//   }
+// }
+/*
+  Response header info:
+  Access-Control-Allow-Origin:*
+  Content-Type:text/json
+  X-Powered-By:nodejs
+*/
+
+// var http  = require('http');
+// var fs    = require('fs');
+var port  = "3000" ;
+// const express = require('express')
+//       // cors = require('cors'),
+//       // morgan = require('morgan'),
+//       // cookieParser = require('cookie-parser'),
+//       bodyParser = require('body-parser'),
+
+// http.createServer(function(request, response) {
+
+//     response.writeHead(200, {
+//         'Content-Type': 'text/json',
+//         'Access-Control-Allow-Origin': '*',
+//         'X-Powered-By':'nodejs'
+//     });
+
+
+//     // fs.readFile('data.json', function(err, content){
+//     //     response.write(content);
+//     //     response.end();
+//     // });
+//     console.log(request.body);
+//     response.write(request.body);
+//     response.end();
+
+// }).listen(port);
+
+// console.log("Listening on port " + port );
+
+
+
+var http = require('http');
+ 
+http.createServer(function(request,response){
+ 
+  response.writeHead(200, {
+      'Content-Type': 'text/json',
+      'Access-Control-Allow-Origin': '*',
+      'X-Powered-By':'nodejs',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Headers': '*'
+  });
+  request.pipe(response);
+ 
+}).listen(port);
